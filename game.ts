04f64b34cda277029d8d34f5eeaa6b01a77f1fb0
Alex Carufel <Carual42@gmail.com>
2023-04-23 15:52:34 -0500

    practice game code implemented

---
+++ game.ts
@@ -0,0 +1,27 @@
+const game = new TicTacToe();
+
+// X plays at (1, 1)
+game.play(1, 1);
+
+// O plays at (0, 0)
+game.play(0, 0);
+
+// X plays at (2, 2)
+game.play(2, 2);
+
+// O plays at (0, 2)
+game.play(0, 2);
+
+// X plays at (0, 1)
+game.play(0, 1);
+
+// O plays at (1, 0)
+game.play(1, 0);
+
+// X plays at (2, 0)
+game.play(2, 0);
+
+// O plays at (1, 2)
+game.play(1, 2);
+
+// X plays at (2, 1
